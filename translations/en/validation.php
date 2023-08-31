faefc1529ca157a97b534d84ffa0442b74863b7b
DevVali <90271108+DevVali@users.noreply.github.com>
2023-08-31 20:47:42 +0200

    fix: fix login and register error messages

---
+++ translations/en/validation.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'numeric' => 'invalid %field!',
+    'notNumeric' => 'invalid %field!',
+    'email' => 'invalid %field!',
+    'url' => 'invalid %field!',
+    'color' => 'invalid %field!',
+    'max' => '%field is too long!',
+    'min' => '%field is too short!',
+    'regex' => 'invalid %field!',
+    'notRegex' => 'invalid %field!',
+    'contains' => 'invalid %field!',
+    'doesntContain' => 'invalid %field!',
+    'missing' => '%field is missing!',
+    'user-exists' => '%field is already in use!',
+    'wrong-login' => 'invalid %field!'
+];
