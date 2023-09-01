1bca94a72a42906bcae5da0d6f22f1e215988ac9
DevVali <90271108+DevVali@users.noreply.github.com>
2023-09-01 10:21:42 +0200

    chore: untrack and ignore css and js files
    This untracks and ignores css and js files generated by Laravel Mix from git.

--- webpack.mix.js
+++ webpack.mix.js
@@ -5,5 +5,5 @@ let mix = require("laravel-mix");
 mix
 	.js("resources/js/app.js", "public/js")
 	.postCss("resources/css/app.css", "public/css", [
-    require("postcss-custom-properties")
+    	require("postcss-custom-properties")
 ]);
